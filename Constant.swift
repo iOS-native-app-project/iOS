b1c43f91932f89e4782ebf8543f686b59529548e
joongkyu-park <jk.park.tech@gmail.com>
2022-03-22 00:26:39 +0900

    Feat: 캘린더 세팅
    - FSCalender 라이브리를 이용하여 캘린더 생성 및 세부 UI 세팅

---
+++ Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  jaksim_ios
+//
+//  Created by Apple on 2022/03/21.
+//
+
+import Foundation
+
+struct K {
+    
+    struct Id{
+        static let calendarViewCellId = "CalendarViewCell"
+    }
+}
--- MyRecord/MyRecordViewController.swift
+++ MyRecord/MyRecordViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import FSCalendar
 
 class MyRecordViewController: UIViewController {
-
+    @IBOutlet weak var calendarView: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        calendarView.delegate = self
+        calendarView.dataSource = self
+        
+        calendarView.appearance.headerDateFormat = "YYYY년 MM월"
+        // M, T, W ...
+        calendarView.appearance.caseOptions = FSCalendarCaseOptions.weekdayUsesSingleUpperCase
+        // 년월에 흐릿하게 보이는 애들 없애기
+        calendarView.appearance.headerMinimumDissolvedAlpha = 0
+        
+        // day 숫자 위치 조정
+        let dayFontSize = calendarView.appearance.titleFont.pointSize
+        calendarView.appearance.titleOffset = CGPoint.init(
+            x: dayFontSize - calendarView.frame.width/7/2,
+            y: -calendarView.rowHeight/2)
+    
+        //calendarView.register(CalendarViewCell.self, forCellReuseIdentifier: K.Id.calendarViewCellId)
+        
     }
     
 
@@ -26,4 +44,8 @@ class MyRecordViewController: UIViewController {
     }
     */
 
+}
+
+extension MyRecordViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
 }
