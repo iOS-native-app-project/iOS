c585c980a99ff0263d01c516b855e2ce075f0e4a
so0nim <308go@naver.com>
2022-03-22 23:29:04 +0900

    feat: record page layout

---
+++ jaksim_ios/RecordInput/RecordInputViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RecordInputViewController.swift
+//  jaksim_ios
+//
+//  Created by 소영 on 2022/03/22.
+//
+
+import UIKit
+
+class RecordInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
