89989f590f0b3d816af5b65679b8c0dd9a7cd099
joongkyu-park <jk.park.tech@gmail.com>
2022-04-27 01:55:44 +0900

    Feat: SearchTermCollectionView에 Rx, MVVM 적용
    - SearchTermCollectionView에 Rx 코드 적용

--- jaksim_ios/MeetingList/View/MeetingListViewController.swift
+++ jaksim_ios/MeetingList/View/MeetingListViewController.swift
@@ -20,19 +20,39 @@ class MeetingListViewController: UIViewController {
     @IBOutlet weak var tableViewTitleLabel: UILabel!
     
     let meetingListViewModel = MeetingListViewModel()
-    var tableViewSectionCount = 0
+    let searchTermListViewModel = SearchTermListViewModel()
+    var searchTermList = [SearchTerm]()
+    var reloadFlag = true
     
     var disposeBag = DisposeBag()
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchBar.searchTextField.layer.cornerRadius = searchBar.bounds.height/2 - 10
         searchBar.searchTextField.layer.masksToBounds = true
         
-        searchTermsCollectionView.dataSource = self
+        //        searchTermsCollectionView.dataSource = self
         searchTermsCollectionView.delegate = self
+        
+        if reloadFlag {
         searchTermsCollectionView.register(UINib(nibName: K.MeetingList.Name.SearchTermsCollectionViewCellXibName, bundle: nil), forCellWithReuseIdentifier: K.MeetingList.Id.SearchTermsCollectionViewCellId)
+        searchTermListViewModel.searchTermListSubject
+            .observe(on: MainScheduler.instance)
+            .do(onNext: { list in
+                self.searchTermList = list
+            })
+            .bind(to: searchTermsCollectionView.rx.items(cellIdentifier: K.MeetingList.Id.SearchTermsCollectionViewCellId, cellType: SearchTermsCollectionViewCell.self)) { index, item, cell in
+            
+                cell.searchTermButton.setTitle(item.name, for: .normal)
+                
+                if index == self.searchTermList.count-1 {
+                    self.reloadFlag = false
+                    self.searchTermsCollectionView.reloadData()
+                }
+            }
+            .disposed(by: disposeBag)
+        }
         
         //meetingListTableView.dataSource = self
         meetingListTableView.delegate = self
@@ -40,17 +60,17 @@ class MeetingListViewController: UIViewController {
         
         collectionViewTitleLabel.font = UIFont(name: K.FontName.PretendardSemiBold, size: 13)
         tableViewTitleLabel.font = UIFont(name: K.FontName.PretendardSemiBold, size: 13)
-
-    
+        
+        
         // 이 bind로 인해 tableView의 dataSource는 필요없어진다.
         meetingListViewModel.meetingListSubject
             .observe(on: MainScheduler.instance)
             .bind(to: meetingListTableView.rx.items(cellIdentifier: K.MeetingList.Id.MeetingListTableViewCellId, cellType: MeetingListTableViewCell.self)) { index, item, cell in
-
+                
                 cell.meetingNameLabel.text = item.name
                 cell.numberOfPeopleLabel.text = "\(item.numberOfpeople)/16"
                 cell.entranceButton.addTarget(self, action: #selector(self.entranceButtonDidTap(_:)), for: .touchUpInside)
-
+                
             }
             .disposed(by: disposeBag)
     }
@@ -58,35 +78,36 @@ class MeetingListViewController: UIViewController {
 
 
 //MARK:- CollectionView Delegate
-extension MeetingListViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension MeetingListViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    // 셀 개수
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 10
-    }
+    //    // 셀 개수
+    //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    //
+    //        return 10
+    //    }
     
     // 셀 사이즈
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let tmpLabel : UILabel = UILabel()
-        tmpLabel.text = "공부"
+
+        let tmpLabel: UILabel = UILabel()
+        tmpLabel.text = searchTermList[indexPath.row].name
+
         
         let width: CGFloat = tmpLabel.intrinsicContentSize.width + 24
         return CGSize(width: width, height: 36)
     }
     
-    // 셀 뷰
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.MeetingList.Id.SearchTermsCollectionViewCellId, for: indexPath) as? SearchTermsCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        
-        cell.searchTermButton.setTitle("공부", for: .normal)
-        
-        return cell
-    }
+    //    // 셀 뷰
+    //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    //
+    //        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.MeetingList.Id.SearchTermsCollectionViewCellId, for: indexPath) as? SearchTermsCollectionViewCell else {
+    //            return UICollectionViewCell()
+    //        }
+    //
+    //        cell.searchTermButton.setTitle("공부", for: .normal)
+    //
+    //        return cell
+    //    }
     
     // 터치업 액션
     //        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -112,27 +133,27 @@ extension MeetingListViewController: UICollectionViewDelegate, UICollectionViewD
 //MARK:- TableView Delegate
 extension MeetingListViewController: UITableViewDelegate { //}, UITableViewDataSource  {
     
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return tableViewSectionCount
-//    }
-//    // There is just one row in every section
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
+    //    func numberOfSections(in tableView: UITableView) -> Int {
+    //        return tableViewSectionCount
+    //    }
+    //    // There is just one row in every section
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        return 1
+    //    }
     
     // Set the spacing between sections
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 10
     }
     
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier:K.MeetingList.Id.MeetingListTableViewCellId, for: indexPath) as! MeetingListTableViewCell
-//
-//        cell.entranceButton.addTarget(self, action: #selector(entranceButtonDidTap(_:)), for: .touchUpInside)
-//
-//        return cell
-//    }
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //
+    //        let cell = tableView.dequeueReusableCell(withIdentifier:K.MeetingList.Id.MeetingListTableViewCellId, for: indexPath) as! MeetingListTableViewCell
+    //
+    //        cell.entranceButton.addTarget(self, action: #selector(entranceButtonDidTap(_:)), for: .touchUpInside)
+    //
+    //        return cell
+    //    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 68
