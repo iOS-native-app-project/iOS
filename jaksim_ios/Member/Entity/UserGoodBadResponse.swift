b0ff1db5acfcd423bf5dd8820d0da546114a8d1b
so0nim <308go@naver.com>
2022-05-16 09:52:26 +0900

    feat: member - 유저 신고/추천 entity

--- jaksim_ios/Member/Entity/UserGoodBadResponse.swift
+++ jaksim_ios/Member/Entity/UserGoodBadResponse.swift
@@ -6,5 +6,13 @@
 //
 
 struct UserGoodBadResponse: Decodable {
-    
+    var success: Bool
+    var statusCode: Int
+    var data: UserGoodBadResult
+}
+
+struct UserGoodBadResult: Decodable {
+    //var generatedMaps: []
+    //var raw: []
+    var affected: Int
 }
