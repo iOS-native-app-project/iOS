026bea901d80b596a22c71fb16ff4de0a7ca7a1c
so0nim <308go@naver.com>
2022-05-16 14:46:54 +0900

    feat: recordInput entity

---
+++ jaksim_ios/RecordInput/Entity/MyRecordResponse.swift
@@ -0,0 +1,22 @@
+//
+//  MyRecordResponse.swift
+//  jaksim_ios
+//
+//  Created by Acryl on 2022/05/16.
+//
+
+struct MyRecordResponse: Decodable {
+    var success: Bool
+    var statusCode: Int
+    var data: [MyRecordResult]?
+}
+
+struct MyRecordResult: Decodable {
+    var id: Int // record id
+    var meetingUserId: Int
+    var meetingId: Int
+    var date: String // 모임 기록 날짜
+    var value: Int // 상세달성수치
+    //var image: "img.png", // 이미지
+    var descript: String // 설명
+}
