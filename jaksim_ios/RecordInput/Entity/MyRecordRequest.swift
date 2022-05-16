70e1d479f1edf249d0dbb78c3795408f1afbc657
so0nim <308go@naver.com>
2022-05-16 15:09:04 +0900

    feat: recordInput entity

---
+++ jaksim_ios/RecordInput/Entity/MyRecordRequest.swift
@@ -0,0 +1,13 @@
+//
+//  MyRecordRequest.swift
+//  jaksim_ios
+//
+//  Created by Acryl on 2022/05/16.
+//
+
+struct MyRecordRequest: Encodable {
+    var date: String
+    var value: String
+    //image": "img.png",
+    var descript: String
+}
